66c168d0db62a5a609801b69ed8d6754b040035c
Dsouth87 <35760144+Dsouth87@users.noreply.github.com>
2019-01-17 12:35:02 -0800

    Update git_practice.py

--- git_practice.py
+++ git_practice.py
@@ -1,3 +1,5 @@
 print "This is practice for using GitHub. This is a change to be commited."
 
 print("This is a sentence added using the github GUI")
+
+print("This is another sentence added using the github GUI")
