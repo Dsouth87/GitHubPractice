031be971ddda31bb7bfe9214c1c3496103bb2454
Derek Southard <derek.southard@ext.airbnb.com>
2019-01-17 13:46:12 -0800

    added a sentence from the feature branch

--- git_practice.py
+++ git_practice.py
@@ -3,3 +3,5 @@ print "This is practice for using GitHub. This is a change to be commited."
 print("This is a sentence added using the github GUI")
 
 print("This is another sentence added using the github GUI")
+
+print("This was added on the feature branch")
